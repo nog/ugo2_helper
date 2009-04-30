fecf7d46bd044a49b79c1d452bccefe554f5d2ed
Shingo Noguchi <precure.sin@gmail.com>
2009-04-30 14:08:53 +0900

    ut=1をベタ書きしたままになってました

--- lib/ugo2_helper.rb
+++ lib/ugo2_helper.rb
@@ -9,7 +9,7 @@ module Ugo2Helper
     ch = options[:ch] || "UTF-8"
     ut = options[:ut] || 1
     ut = ut.to_s
-    result =  "http://#{server}/?u=#{user}&h=#{hash}&guid=ON&ut=1"
+    result =  "http://#{server}/?u=#{user}&h=#{hash}&guid=ON&ut=#{ut}"
     result += "&qM=#{phpurlencode(request.referer ? request.referer : '')}|AzR|#{request.port.to_s}|#{phpurlencode(request.host.to_s)}|#{phpurlencode(request.request_uri.to_s)}|Y|"
     result += "&ch=#{ch}"
     result += "&sb=#{phpurlencode(title)}"
