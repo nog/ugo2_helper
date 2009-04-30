3ac3daf1835bc7e9138cbe828f4e1e439b0b6a6b
Shingo Noguchi <precure.sin@gmail.com>
2009-04-30 14:49:49 +0900

    default optionを切り出した テスト追加した

--- lib/ugo2_helper.rb
+++ lib/ugo2_helper.rb
@@ -6,9 +6,10 @@ module Ugo2Helper
   end
 
   def make_ugo2_image_url(server, user, hash, title, options={})
-    ch = options[:ch] || "UTF-8"
-    ut = options[:ut] || 1
-    ut = ut.to_s
+    options = ugo2_default_options.merge(options)
+    ut = options[:ut].to_s
+    ch = options[:ch]
+
     result =  "http://#{server}/?u=#{user}&h=#{hash}&guid=ON&ut=#{ut}"
     result += "&qM=#{phpurlencode(request.referer ? request.referer : '')}|AzR|#{request.port.to_s}|#{phpurlencode(request.host.to_s)}|#{phpurlencode(request.request_uri.to_s)}|Y|"
     result += "&ch=#{ch}"
@@ -18,12 +19,23 @@ module Ugo2Helper
   end
 
   def ugo2_tag(server, user, hash, title, options={})
+    options = ugo2_default_options.merge(options)
+
     image_url = make_ugo2_image_url(server, user, hash, title, options)
+
     size = (options[:ut].to_s == "2") ? "1x1" : "72x16"
+
     link_to_if(
       options[:ut].to_s == "1",
       image_tag(image_url, :border => '0', :size => size, :alt => '携帯アクセス解析'),
       'http://ugo2.jp/m/'
     )
+  end
+
+  def ugo2_default_options
+    {
+      :ut => 1,
+      :ch => "UTF-8"
+    }
   end
 end
--- test/ugo2_helper_test.rb
+++ test/ugo2_helper_test.rb
@@ -5,6 +5,7 @@ require 'action_controller/test_process'
 require File.expand_path(File.dirname(__FILE__) + "/../lib/ugo2_helper")
 
 class Ugo2HelperTest < ActiveSupport::TestCase
+  include ActionView::Helpers
   include Ugo2Helper
 
   def setup
@@ -33,6 +34,59 @@ class Ugo2HelperTest < ActiveSupport::TestCase
       "http://b03.ugo2.jp/?u=0000000&h=aaaaaa&guid=ON&ut=1&qM=http%3A%2F%2Fyahoo.co.jp%2F|AzR|80|test.host|%2F|Y|&ch=UTF-8&sb=%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AE%E3%82%BF%E3%82%A4%E3%83%88%E3%83%AB%E3%81%A0%E3%82%88",
       result
     )
+    assert_match "ut=1", result
+    assert_match "ch=UTF-8", result
+  end
+
+  test "ugo2_tag" do
+    result = ugo2_tag(
+      "b03.ugo2.jp",
+      "0000000",
+      "aaaaaa",
+      "ページのタイトルだよ"
+    )
+
+    assert_equal result, %Q{<a href="http://ugo2.jp/m/"><img alt="携帯アクセス解析" border="0" height="16" src="http://b03.ugo2.jp/?u=0000000&amp;h=aaaaaa&amp;guid=ON&amp;ut=1&amp;qM=|AzR|80|test.host|%2F|Y|&amp;ch=UTF-8&amp;sb=%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AE%E3%82%BF%E3%82%A4%E3%83%88%E3%83%AB%E3%81%A0%E3%82%88" width="72" /></a>}
+    assert_match "ut=1", result
+    assert_match "ch=UTF-8", result
+  end
+
+
+  test "ugo2_tagがut=2だったurlにut=2が渡される" do
+    result = ugo2_tag(
+      "bxx.ugo2.jp", "0000000", "abcdef", "タイトル",
+      :ut => 2
+    )
+    assert_match "ut=2", result
+  end
+
+  test "ugo2_tagがut=2だった時リンクを出力しない" do
+    result = ugo2_tag(
+      "bxx.ugo2.jp", "0000000", "abcdef", "タイトル",
+      :ut => 2
+    )
+
+    assert !result.match(/<a /)
+  end
+
+
+  test "make_ugo2_image_urlがut=2だった時ut=2が出力される" do
+    result = ugo2_tag(
+      "bxx.ugo2.jp", "0000000", "abcdef", "タイトル",
+      :ut => 2
+    )
+
+    assert !!result.match(%Q{ut=2})
+  end
+
+  test "make_ugo2_image_urlがut=2だった時画像のサイズは1x1" do
+    result = ugo2_tag(
+      "bxx.ugo2.jp", "0000000", "abcdef", "タイトル",
+      :ut => 2
+    )
+
+    assert !!result.match(%Q{height="1"})
+    assert !!result.match(%Q{width="1"})
   end
 
   def request
