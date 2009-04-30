192613861c39bdda46fbaabf17fbc815c57d7b4f
Shingo Noguchi <precure.sin@gmail.com>
2009-04-30 12:01:35 +0900

    法人ユーザー対応、chをoptionsに移動

--- lib/ugo2_helper.rb
+++ lib/ugo2_helper.rb
@@ -5,7 +5,10 @@ module Ugo2Helper
     URI.encode(str, /[^a-zA-Z\d\-\_\.]/n)
   end
 
-  def make_ugo2_image_url(server, user, hash, title, ch="UTF-8")
+  def make_ugo2_image_url(server, user, hash, title, options={})
+    ch = options[:ch] || "UTF-8"
+    ut = options[:ut] || 1
+    ut = ut.to_s
     result =  "http://#{server}/?u=#{user}&h=#{hash}&guid=ON&ut=1"
     result += "&qM=#{phpurlencode(request.referer ? request.referer : '')}|AzR|#{request.port.to_s}|#{phpurlencode(request.request_uri.to_s)}|Y|"
     result += "&ch=#{ch}"
@@ -14,10 +17,11 @@ module Ugo2Helper
     return result
   end
 
-  def ugo2_tag(server, user, hash, title, ch="UTF-8")
-    image_url = make_ugo2_image_url(server, user, hash, title, ch)
+  def ugo2_tag(server, user, hash, title, options={})
+    image_url = make_ugo2_image_url(server, user, hash, title, options)
+    size = (options[:ut].to_s == "2") ? "1x1" : "72x16"
     link_to(
-      image_tag(image_url, :border => '0', :size => '72x16', :alt => '携帯アクセス解析'),
+      image_tag(image_url, :border => '0', :size => size, :alt => '携帯アクセス解析'),
       'http://ugo2.jp/m/'
     )
   end
